766f5ace0a3f75c6bbe30b5b42dfa841cd6e0651
Hussain Babatunde <tudemine@gmail.com>
2023-10-02 11:23:39 -0700

    completed the history and logout screens

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { FlatList, SafeAreaView, View } from "react-native";
 import { Text } from "react-native";
+import { supabase } from "../utils/supabase";
+import { ActivityIndicator } from "react-native";
 
 const HistoryScreen = () =>{
 
-    const [data, setData] = useState(['My name', 'Tunde'])
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(()=>{
         const getUser = async() =>{
+            setLoading(true)
           try {
             const res = await fetch('https://spitfire-interractions.onrender.com/api/auth/@me');
             const response = await res.json()
         
             // Add this line to inspect the response
-            console.log('Response:', response);
+            // console.log('Response:', response);
+            if(response){
+            let {data} = await supabase.from('chats')
+            .select('*')
+            .eq('user_id', response?.data?.id)
+            // console.log(data?.length)
+            setData(data)
+            }
         
             // setLoading(false);
           } catch (error) {
             console.error('Error logging out', error);
           }
+          setLoading(false)
         }
         getUser()
       },[])
@@ -28,15 +40,20 @@ const HistoryScreen = () =>{
                 <Text style={{ fontSize: 23, marginTop: 20, color:'#C67C4E'}}>History</Text>
             </View>
             <View style={{marginTop: 50, flex: 1}}>
+            {loading? 
+            <View style={{flex: 1, justifyContent:'center', alignItems:'center'}}>
+            <ActivityIndicator size='large' color='black' />
+            </View>
+            :
             <FlatList
             style={{paddingHorizontal: 20, flex: 1}}
                     data={data}
                     renderItem={({ item }) => (                        
-            <Text style={{borderWidth: 2, borderColor:'black', textAlign:'center', padding: 10, marginTop: 10}}>History Screen</Text>
+            <Text style={{borderWidth: 2, borderColor:'black', textAlign:'center', padding: 10, marginTop: 10}}>{item?.chat[0]?.question}</Text>
                       )}
                     keyExtractor={(item, index) => index.toString()} // Use index as key
       // Other props like numColumns, horizontal, etc. can be added here
-                    />
+                    />}
             </View>
         </SafeAreaView>
     )
